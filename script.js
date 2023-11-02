506de34a1e0d06233802ae579fc511c71398f3f5
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2023-11-02 12:00:41 +0100

    JS file added to html

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var randomNumber1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
 var randomNumber2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-
+console.log(randomNumber1);
+console.log(randomNumber2);
 var randomImageSrc1 = "./images/dice" + randomNumber1 + ".png";
 var randomImageSrc2 = "./images/dice" + randomNumber2 + ".png";
+console.log(randomImageSrc1);
+console.log(randomImageSrc2);
 
 document.querySelector(".img1").setAttribute("src", randomImageSrc1);
 document.querySelector(".img2").setAttribute("src", randomImageSrc2);
