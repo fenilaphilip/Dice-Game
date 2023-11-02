75c4533598e6fc564b1c03a639e6e3ce4b9d5e8d
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2023-11-02 12:35:53 +0100

    DOM applied to h1

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ console.log(randomImageSrc2);
 
 document.querySelector(".img1").setAttribute("src", randomImageSrc1);
 document.querySelector(".img2").setAttribute("src", randomImageSrc2);
+
+var showWinner = "";
+if (randomNumber1 > randomNumber2) {
+  showWinner = " Player 1 won";
+} else if (randomNumber2 > randomNumber1) {
+  showWinner = " Player 2 won";
+} else {
+  showWinner = "Draw";
+}
+
+document.querySelector("h1").innerHTML = showWinner;
